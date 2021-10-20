356f4a50301730df515b5ce6c9a52daf74389916
Gabriel Nau <gabriel.nau@gmail.com>
2021-10-20 14:40:47 +0200

    Fix Integrant repl setup in example and make it work without javaagent

--- README.md
+++ README.md
@@ -59,8 +59,8 @@ bin/kaocha
 When using Agent, you get the following constraints / caveats:
 
 - "direct usage of the OpenTelemetry SDK is not supported when running agent" TODO: source
-- Span implementation when using autoinstrumentation java agent differs from the one from the SDK
-  - TODO: ask about this on cncf slack, Span doesn't implement Context interface in agent but it does in api
+~~- Span implementation when using autoinstrumentation java agent differs from the one from the SDK
+  - TODO: ask about this on cncf slack, Span doesn't implement Context interface in agent but it does in api~~
 - tracer is built by the agent, thus need to use auto configuration or ?
 - Javaagent version vs SDK version, see this slack message:
   > We support all OTel APIs up to the version of the javaagent, but not necessarily newer. The assumption is it’s generally at least as easy to update the javaagent version as an app. And while not impossible for all cases, it’s hard to guarantee compatibility with something that doesn’t exist yet
@@ -201,6 +201,7 @@ We have to differentiate 2 use cases:
   - don't depend on latest clojure ? 
   - check for build, CI for open source
   - how to publish to clojars
+  - how to publish to maven central
   - see clojurians announcements and tracing channels to gather feedback + https://clojureverse.org/c/showcase/your-projects-and-libraries/35
 - Bookmars:
   - Programmatic injection of agent https://cloud-native.slack.com/archives/C0150QF88FL/p1632249888088300?thread_ts=1631980158.059400&cid=C0150QF88FL
--- examples/jetty_example.clj
+++ examples/jetty_example.clj
@@ -1,7 +1,8 @@
 (ns jetty-example
-  (:require [reitit.ring :as ring]
+  (:require [integrant.core :as ig]
+            [integrant.repl :refer [clear go halt prep init reset reset-all]]
+            [reitit.ring :as ring]
             [ring.adapter.jetty :as jetty]
-            [integrant.core :as ig]
             [muuntaja.interceptor]
             [reitit.interceptor.sieppari]
             [sieppari.async.core-async]
@@ -15,63 +16,82 @@
             [next.jdbc.connection :as connection])
   (:import (io.opentelemetry.api GlobalOpenTelemetry)
            (io.opentelemetry.context Context)
-           (com.zaxxer.hikari HikariDataSource)))
+           (com.zaxxer.hikari HikariDataSource)
+           (io.opentelemetry.sdk.trace SdkTracerProvider)
+           (io.opentelemetry.sdk.trace.export SimpleSpanProcessor)
+           (io.opentelemetry.exporters.logging LoggingSpanExporter)
+           (io.opentelemetry.sdk OpenTelemetrySdk)
+           (io.opentelemetry.context.propagation ContextPropagators)
+           (io.opentelemetry.api.trace.propagation W3CTraceContextPropagator)))
 
 ;; tracer instance
-(def tracer (GlobalOpenTelemetry/getTracer "jetty-handler"))
+(def tracer-atom (atom nil))
+
+(defn query-db [db]
+  (let [context (context/current)]
+    (a/thread
+      (println "thread" context)
+      (with-open [_ (context/make-current! context)]
+        (let [span   (-> (span/new-builder @tracer-atom "CHILD") span/start)
+              result (jdbc/execute! db ["SELECT 'hello world'"])]
+          (span/end span)
+          result)))))
+
 
 (defn handler
   ([db request]
-   (let [span (-> (span/new-builder tracer "FOO 1")
+   (let [span (-> (span/new-builder @tracer-atom "FOO 2")
                 (span/set-kind :server)
                 span/start)]
-     (Thread/sleep 100)
-     (with-open [_ (-> (Context/current) (.with span) .makeCurrent)]
-       (let [context (context/current)]
-         (a/thread
-           (with-open [_ (context/make-current! context)]
-             (jdbc/execute! db ["SELECT 'hello world'"]))
-           (span/end span)
-           {:status 200, :headers {}, :body "Hello World"}))))))
+     ;(with-open [_ (-> (Context/current) (.with span) .makeCurrent)])
+     (with-open [_ (.makeCurrent span)]
+       (let [res (query-db db)]
+         (a/go
+           (println (a/<! res)))
+         (span/end span)
+         {:status 200, :headers {}, :body "Hello World"})))))
 
 ;; Integrant wiring
 
-(defmethod ig/init-key :example/jetty [_ {:keys [port join? handler async?]}]
+(defmethod ig/init-key ::jetty [_ {:keys [port join? handler async?]}]
   (jetty/run-jetty handler {:port port :join? join? :async? async?}))
 
-(defmethod ig/halt-key! :example/jetty [_ server]
-  (.join server))
+(defmethod ig/halt-key! ::jetty [_ server]
+  (.stop server))
 
-(defmethod ig/init-key :example/handler [_ {:keys [db]}]
+(defmethod ig/init-key ::handler [_ {:keys [db]}]
   (ring/ring-handler
     (ring/router
       ["/ping" {:get {:handler (partial handler db)}}])))
 
-(defmethod ig/init-key :example/db [_ _]
+(defmethod ig/init-key ::tracer [_ _]
+  (let [tracer-provider (-> (doto (SdkTracerProvider/builder)
+                              (.addSpanProcessor (SimpleSpanProcessor/create (LoggingSpanExporter.))))
+                          .build)
+        otel            (-> (doto (OpenTelemetrySdk/builder)
+                              (.setTracerProvider tracer-provider)
+                              (.setPropagators (ContextPropagators/create (W3CTraceContextPropagator/getInstance))))
+                          .buildAndRegisterGlobal)
+        tracer          (.getTracer otel "my-custom-tracer" "1.0.0")]
+    (reset! tracer-atom tracer)
+    tracer))
+
+
+(defmethod ig/init-key ::db [_ _]
   (let [ds (connection/->pool HikariDataSource {:dbtype "postgres" :dbname "postgres" :username "postgres" :password "changeme"})]
     (.close (jdbc/get-connection ds))                       ;; initializes the pool and performs a validation check:
     ds))
 
-(defmethod ig/halt-key! :example/db [_ datasource]
+(defmethod ig/halt-key! ::db [_ datasource]
   (.close datasource))
 
 (def system-config
-  {:example/jetty   {:port    3000
-                     :join?   false
-                     :async?  false
-                     :handler (ig/ref :example/handler)}
-   :example/db      {}
-   :example/handler {:db (ig/ref :example/db)}})
+  {::jetty   {:port    3000
+              :join?   false
+              :async?  false
+              :handler (ig/ref ::handler)}
+   ::db      {}
+   ::tracer  {}
+   ::handler {:db (ig/ref ::db)}})
 
-
-(def system (ig/init system-config))
-;(defn start []
-;   (reset! runtime-system (ig/init system-config))
-;  :ok)
-
-(defn stop []
-  (ig/halt! system))
-
-(comment
-  (start))
-
+(integrant.repl/set-prep! (constantly system-config))
